f0926407a792679ee75dfa5e3f288fe2d1619396
Jacob Pixler <106707326+yoyocharlie@users.noreply.github.com>
2024-09-30 15:03:31 -0600

    Removed project for demo brevity

--- src/components/projects/data.tsx
+++ src/components/projects/data.tsx
@@ -77,34 +77,5 @@ export const timelineData: TimelineEntry[] = [
         text: `Seamlessly customize the portfolio to fit your unique style and preferences. With support for easy integration into various data sources, this opinionated template is designed to simplify the process of building a single-page application (SPA) portfolio that truly reflects your work.`,
       },
     },
-  },
-  {
-    title: "Simple Joys (WIP)",
-    projectUrls: {
-      site: {
-        url: "https://simplejoysmedia.com",
-        icon: <RiExternalLinkLine size={20} />,
-      },
-    },
-    videoUrl: "/images/simple-joys-video.mp4",
-    description: `A cinematography portfolio built with Next.js and Sanity.io for fast page speeds and quick delivery of high quality assets.`,
-    tech: [
-      "TypeScript",
-      "Next.js",
-      "TailwindCSS",
-      "Framer Motion",
-      "tRPC",
-      "Sanity",
-    ],
-    cards: {
-      a: {
-        title: "Fast & Responsive Performance",
-        text: `Built with Next.js, this cinematography portfolio ensures lightning-fast page speeds and smooth navigation. High-quality video and image assets load efficiently, offering a seamless viewing experience without compromising on performance.`,
-      },
-      b: {
-        title: "Powered by Sanity.io",
-        text: `Leveraging Sanity.io, this portfolio delivers dynamic content and high-resolution assets with ease. Update your work in real-time and enjoy the flexibility of a powerful CMS, ensuring that your portfolio stays current and visually stunning.`,
-      },
-    },
-  },
+  }
 ];
